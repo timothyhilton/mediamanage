f9cefedbecfc33b1439913d3a2d65e73d11691c5
timothyhilton <recore444@gmail.com>
2023-07-10 13:37:45 +1000

    converted old php hero page to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import VideoUpload from './components/VideoUpload';
 import './App.css'
 import { useState } from 'react';
+import VideoUpload from './components/VideoUpload';
+import FrontPage from './pages/FrontPage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   
-  return (
-    <>        
-        <VideoUpload />
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<FrontPage />} />
+                <Route path="/video" element={<VideoUpload />} />
+            </Routes>
+        </>
+    )
 }
 
 export default App
