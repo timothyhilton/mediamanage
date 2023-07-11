ed860b411c7e7f049f59f674c49e54a823936d98
timothyhilton <recore444@gmail.com>
2023-07-11 17:47:35 +1000

    cleaned code and fixed errors

--- src/components/FrontPage/FAQ.tsx
+++ src/components/FrontPage/FAQ.tsx
@@ -4,15 +4,11 @@ import { AnimatePresence, motion } from "framer-motion";
 function FAQ(props: any){
     const [expanded, setExpanded] = useState(false);
 
-    useEffect(() => {
-        console.log(expanded);
-    }, [expanded]);
-
     return(
         <div className="relative overflow-hidden border-b border-gray-100 select-none">
             <h4 onClick={() => setExpanded(!expanded)} className="flex items-center justify-between px-2 text-lg font-medium text-gray-700 cursor-pointer sm:text-xl md:text-2xl py-7 hover:text-gray-900">
                 <span className="">{props.question}</span>
-                <svg className="w-6 h-6 mr-2 transition-all duration-200 ease-out transform rotate-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+                <svg className="w-6 h-6 mr-2 transition-all duration-200 ease-out transform rotate-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7"></path></svg>
             </h4>
             
             <AnimatePresence>
