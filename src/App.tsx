394980ea9cd9fa22360cfe16ce0e96a9469a8392
timothyhilton <recore444@gmail.com>
2023-07-13 18:54:22 +1000

    user authentication works

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,21 @@ import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
     return (
         <>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={<FrontPage />} />
-                <Route path="/video" element={<VideoUpload />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/home" element={<HomePage />} />
-            </Routes>
+            <body className="bg-gray-50">
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<FrontPage />} />
+                    <Route path="/video" element={<VideoUpload />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/home" element={<HomePage />} />
+                </Routes>
+            </body>
         </>
     )
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,69 +1,107 @@
-function LoginPage(){
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"
+import ButtonLoading from "../components/ButtonLoading";
+import ErrorMessage from "../components/Auth/ErrorMessage";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+function RegisterPage(){
+    const [data, setData] = useState({
+        email: '',
+        password: ''
+    });
+    const [registerButtonContents, setRegisterButtonContents] = useState(<p>Sign in</p>);
+    const [errorMessageDiv, setErrorMessageDiv] = useState(<div />);
+    const navigate = useNavigate();
+    const axiosErrorMessage = AxiosError;
+
+    function handleFormChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setData(data => ({...data, [event.target.id]: event.target.value}));
+        console.log(data);
+    }
+
+    function handleFormSubmit(){
+        axios.post(`${apiUrl}/auth/login`, data, { headers: {"Content-Type": "application/json"}})
+            .then(res => console.log(res))
+            .catch(err => handleError(err))
+    }
+
+    function handleError(err: any){
+        console.log(err);
+    }
+
+    function updateErrElements(errors: string[]){
+        setErrorMessageDiv(<ErrorMessage errors={errors} />);
+            setRegisterButtonContents(<p>Register</p>);
+    }
+
     return(
-        <div className="flex flex-col justify-center py-20 sm:px-6 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <h2 className="mt-6 text-3xl font-extrabold leading-9 text-center text-gray-900 lg:text-5xl">
+        <>
+            <div className="sm:mx-auto sm:w-full sm:max-w-md sm:pt-10 mt-6">
+                <h2 className="text-3xl font-extrabold leading-9 text-center text-gray-900 sm:mt-6 lg:text-5xl">
                     Sign in Below
                 </h2>
                 <p className="mt-4 text-sm leading-5 text-center text-gray-600 max-w">
-                    or, you can
-                    <a href="{{ route('register') }}" className="font-medium transition duration-150 ease-in-out text-wave-600 hover:text-wave-500 focus:outline-none focus:underline">
+                    or, you can{" "}
+                    <a href="/register" className="font-medium transition duration-150 ease-in-out text-blue-600 hover:text-blue-500 focus:outline-none focus:underline">
                         signup here
                     </a>
                 </p>
             </div>
 
-            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-                <div className="px-4 py-8 bg-white border shadow border-gray-50 sm:rounded-lg sm:px-10">
-                    <form action="#" method="POST">
-                        <div>
-                                <label className="block text-sm font-medium leading-5 text-gray-700">Username</label>
-                                <div className="mt-1 rounded-md shadow-sm">
-                                    <input id="username" type="username" name="username" required className="w-full form-input" />
-                                </div>
-                                <label className="block text-sm font-medium leading-5 text-gray-700">Email address</label>
-                                <div className="mt-1 rounded-md shadow-sm">
-                                    <input id="email" type="email" name="email" required className="w-full form-input" />
-                                </div>
-                        </div>
+            {errorMessageDiv}
 
-                        <div className="mt-6">
-                            <label className="block text-sm font-medium leading-5 text-gray-700">
-                                Password
-                            </label>
-                            <div className="mt-1 rounded-md shadow-sm">
-                                <input id="password" type="password" name="password" required className="w-full form-input" />
-                            </div>
-                        </div>
-
-                        <div className="flex items-center justify-between mt-6">
-                            <div className="flex items-center">
-                                <input id="remember" name="remember" type="checkbox" className="text-indigo-600 border-0 border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50 rounded-xl" />
-                                <label className="block ml-2 text-sm leading-5 text-gray-900">
-                                    Remember me
+            <div className="flex flex-col justify-center pb-10 sm:pb-20 sm:px-6 lg:px-8">
+                <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                    <div className="px-4 py-8 bg-white border shadow border-gray-50 sm:rounded-lg sm:px-10">
+                        <form role="form" method="POST" action="hi">
+                            <div className="">
+                                <label className="block text-sm font-medium leading-5 text-gray-700">
+                                    Email Address
                                 </label>
+                                <div className="mt-1 rounded-md shadow-sm">
+                                    <input id="email" type="email" name="email" onChange={handleFormChange} required className="w-full form-input"/>
+                                </div>
                             </div>
 
-                            <div className="text-sm leading-5">
-                                <a href="{{ route('password.request') }}" className="font-medium transition duration-150 ease-in-out text-wave-600 hover:text-wave-500 focus:outline-none focus:underline">
-                                    Forgot your password?
-                                </a>
+                            <div className="mt-6">
+                                <label className="block text-sm font-medium leading-5 text-gray-700">
+                                    Password
+                                </label>
+                                <div className="mt-1 rounded-md shadow-sm">
+                                    <input id="password" type="password" name="password" onChange={handleFormChange} required className="w-full form-input"/>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="mt-6">
-                            <span className="block w-full rounded-md shadow-sm">
-                                <button type="submit" className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out border border-transparent rounded-md bg-wave-600 hover:bg-wave-500 focus:outline-none focus:border-wave-700 focus:shadow-outline-wave active:bg-wave-700">
-                                    Sign in
-                                </button>
-                            </span>
-                        </div>
-                    </form>
+                            <div className="flex items-center justify-between mt-6">
+                                <div className="flex items-center">
+                                    <input id="remember" name="remember" type="checkbox" className="text-indigo-600 border-0 border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50" />
+                                    <label className="block ml-2 text-sm leading-5 text-gray-900">
+                                        Remember me
+                                    </label>
+                                </div>
 
+                                <div className="text-sm leading-5">
+                                    <a href="{{ route('password.request') }}" className="font-medium transition duration-150 ease-in-out text-blue-600 hover:text-blue-500 focus:outline-none focus:underline">
+                                        Forgot your password?
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="flex flex-col items-center justify-center text-sm leading-5">
+                                <span className="block w-full mt-5 rounded-md shadow-sm">
+                                    <button type="button" onClick={handleFormSubmit} className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out border border-transparent rounded-md bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700">
+                                        {registerButtonContents}
+                                    </button>
+                                </span>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
-export default LoginPage
+export default RegisterPage
