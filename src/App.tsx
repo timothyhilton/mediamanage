ee7994fba76fcd5b5aa722def1c0f566244905d7
tim h <recore444@gmail.com>
2023-07-15 20:26:22 +1000

    restricted access to /home without jwt

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import { useEffect, useState } from 'react';
 import VideoUpload from './components/VideoUpload';
 import FrontPage from './pages/FrontPage';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
@@ -12,19 +12,31 @@ import { AuthToken } from './models/AuthToken';
 
 function App() {
     const [token, setToken] = useState("");
+    const [home, setHome] = useState(<HomePage />);
 
     useEffect(() => {
-        if(token != ""){
-            let decodedToken = jwt_decode<AuthToken>(token);
-            let currentDate = new Date();
-
-            if (decodedToken.exp * 1000 < currentDate.getTime()) {
-                console.log("Token expired.");
-            } else {
-                console.log("Valid token");   
-            }
+        if(isTokenValid()){
+            setHome(<HomePage />);
+        }
+        else{
+            setHome(<Navigate to="/login" />);
         }
     }, [token])
+
+    function isTokenValid(): boolean{
+        if(token == "") { return false; }
+        
+        let decodedToken = jwt_decode<AuthToken>(token);
+        let currentDate = new Date();
+
+        if (decodedToken.exp * 1000 < currentDate.getTime()) {
+            console.log("Token expired");
+            return true;
+        } else {
+            console.log("Valid token");
+            return true;
+        }
+    }
 
     return (
         <>
@@ -34,7 +46,7 @@ function App() {
                     <Route path="/" element={<FrontPage />} />
                     <Route path="/register" element={<RegisterPage />} />
                     <Route path="/login" element={<LoginPage setToken={setToken}/>} />
-                    <Route path="/home" element={<HomePage token={token} />} />
+                    <Route path="/home" element={home}/>
                 </Routes>
             </div>
         </>
