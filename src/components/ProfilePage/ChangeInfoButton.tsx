aa80d9392535454d18a66c9fe1439554dbec9673
timothyhilton <recore444@gmail.com>
2023-08-28 16:06:41 +1000

    refactored with react-modal

--- src/components/ProfilePage/ChangeInfoButton.tsx
+++ src/components/ProfilePage/ChangeInfoButton.tsx
@@ -2,67 +2,73 @@ import { useState } from "react";
 import { UserInfo } from "../../models/UserInfo";
 import { AnimatePresence, motion } from "framer-motion";
 import { useForm } from "react-hook-form";
+import ButtonLoading from "../ButtonLoading";
+import Modal from 'react-modal';
 
 function ChangeInfoButton({ username, email }: UserInfo){
-    const [isOpen, setIsOpen] = useState(false);
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = (data: any) => console.log(data);
+    const [isModalOpen, setisModalOpen] = useState(false);
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [submitButtonContent, setSubmitButtonContents] = useState(<p>Submit Changes</p>);
 
-    function toggle() {
-        setIsOpen(!isOpen);
+    function toggleModal() {
+        setisModalOpen(!isModalOpen);
     }
 
-    console.log(watch("example"));
+    function onSubmit(info: UserInfo){
+        console.log(info)
+        setSubmitButtonContents(<ButtonLoading />);
+    }
 
     return(
         <div>
             <button 
-                onClick={toggle}
+                onClick={toggleModal}
                 className="flex justify-center w-fit px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out border border-transparent rounded-md bg-slate-600 hover:bg-slate-500 focus:outline-none focus:border-slate-700 focus:shadow-outline-blue active:bg-slate-700"
             >
                 Change account info
             </button>
 
-            <AnimatePresence>
-                {isOpen &&
-                    <motion.div
-                        initial={{opacity: 0}}
-                        animate={{opacity: 1}}
-                        transition={{ duration: 0.3 }}
-                        exit={{ opacity: 0, transition:{duration: 0.2}}}
-                    > 
-                        <div onClick={toggle} className="p-20 border-4 border-white fixed top-0 left-0 bottom-0 right-0 w-full h-screen bg-white bg-opacity-75"></div>
+            <Modal
+                isOpen={isModalOpen}
+                onRequestClose={toggleModal}
+                contentLabel="Example Modal"
+                className={"bg-white sm:w-full flex flex-col sm:mx-auto sm:w-full sm:max-w-md"}
+            >
+                <div className="px-4 py-8 bg-white border shadow border-gray-50 sm:rounded-lg sm:px-10">
 
-                        <div className="absolute flex flex-col justify-center pb-10 sm:pb-20 sm:px-6 lg:px-8">
-                            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-                                <div className="px-4 py-8 bg-white border shadow border-gray-50 sm:rounded-lg sm:px-10">
-                                    <form onSubmit={handleSubmit(onSubmit)}>
-                                        <div className="">
-                                            <label className="block text-sm font-medium leading-5 text-gray-700">
-                                                Username
-                                            </label>
-                                            <div className="mt-1 rounded-md shadow-sm">
-                                                <input defaultValue={username} {...register("username")} className="w-full form-input"/>
-                                            </div>
-                                        </div>
+                    <h1 className="text-2xl font-bold mb-6">
+                        Change account settings below
+                    </h1>
 
-                                        <div className="mt-6">
-                                            <label className="block text-sm font-medium leading-5 text-gray-700">
-                                                Email
-                                            </label>
-                                            <div className="mt-1 rounded-md shadow-sm">
-                                                <input defaultValue={email} {...register("email")} className="w-full form-input"/>
-                                            </div>
-                                        </div>
-
-                                        <input type="submit" />
-                                    </form>
-                                </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="">
+                            <label className="block text-sm font-medium leading-5 text-gray-700">
+                                Username
+                            </label>
+                            <div className="mt-1 rounded-md shadow-sm">
+                                <input defaultValue={username} {...register("username")} className="w-full form-input"/>
                             </div>
                         </div>
-                    </motion.div>
-                }
-            </AnimatePresence>
+
+                        <div className="mt-6">
+                            <label className="block text-sm font-medium leading-5 text-gray-700">
+                                Email
+                            </label>
+                            <div className="mt-1 rounded-md shadow-sm">
+                                <input defaultValue={email} {...register("email")} className="w-full form-input"/>
+                            </div>
+                        </div>
+
+                        <div className="mt-3 flex flex-col items-center justify-center text-sm leading-5">
+                            <span className="block w-full mt-5 rounded-md shadow-sm">
+                                <button type="submit" className="font-semibold flex justify-center w-full px-4 py-2 text-sm text-white transition duration-150 ease-in-out border border-transparent rounded-md bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700">
+                                    {submitButtonContent}
+                                </button>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+            </Modal>
         </div>
     )
 }
